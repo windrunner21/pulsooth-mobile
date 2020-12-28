6b48a4e11c573b1687c2f1e9b32fb85c947bc775
Imran <haciyev008@gmail.com>
2020-12-28 19:33:01 +0400

    landing page changed

--- lib/ui/landing-page.dart
+++ lib/ui/landing-page.dart
@@ -1,5 +1,7 @@
 import 'dart:async';
+import 'dart:ui';
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 import 'package:pulsooth_mobile/ui/auth-components/launch-page.dart';
 
 class LandingPage extends StatefulWidget {
@@ -26,12 +28,63 @@ class _LandingPageState extends State<LandingPage> {
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.black,
-      body: Padding(
-        padding: const EdgeInsets.symmetric(horizontal: 15, vertical: 40),
-        child: Center(
-          child: Image(
-            image: AssetImage('lib/assets/logo.png'),
-            height: MediaQuery.of(context).size.height / 5,
+      body: Container(
+        decoration: BoxDecoration(
+          image: DecorationImage(
+              image: AssetImage('lib/assets/logo.jpg'), fit: BoxFit.cover),
+        ),
+        child: Align(
+          alignment: Alignment.centerLeft,
+          child: Container(
+            decoration: BoxDecoration(
+              boxShadow: [
+                BoxShadow(
+                  blurRadius: 24,
+                  spreadRadius: 16,
+                  color: Colors.black.withOpacity(0.2),
+                ),
+              ],
+            ),
+            child: ClipRRect(
+              child: BackdropFilter(
+                filter: ImageFilter.blur(sigmaX: 16, sigmaY: 16),
+                child: Container(
+                  padding: EdgeInsets.symmetric(horizontal: 15),
+                  height: MediaQuery.of(context).size.height,
+                  width: MediaQuery.of(context).size.width / 1.8,
+                  decoration: BoxDecoration(
+                    color: Colors.white.withOpacity(0.3),
+                    border: Border.all(
+                      width: 1,
+                      color: Colors.white.withOpacity(0.1),
+                    ),
+                  ),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                    children: [
+                      Text(
+                        'PULSOOTH',
+                        style: GoogleFonts.bungeeShade(
+                          textStyle: TextStyle(
+                            color: Colors.black,
+                            fontSize: 28,
+                          ),
+                        ),
+                      ),
+                      Text(
+                        'Get your product samples for free',
+                        style: TextStyle(
+                          fontWeight: FontWeight.bold,
+                          color: Colors.white,
+                          fontSize: 40,
+                        ),
+                      )
+                    ],
+                  ),
+                ),
+              ),
+            ),
           ),
         ),
       ),
--- lib/ui/product-components/product-page.dart
+++ lib/ui/product-components/product-page.dart
@@ -1,5 +1,4 @@
 import 'dart:ui';
-
 import 'package:flutter/material.dart';
 import 'package:flutter_icons/flutter_icons.dart';
 import 'package:pulsooth_mobile/ui/product-components/bag-page.dart';
